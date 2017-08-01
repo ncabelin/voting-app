8c213fba6ebb206a10363572bca9afa6e3462fb8
Michael Cabelin <ncabelin@gmail.com>
2017-08-01 14:40:03 -0700

    add twitter auth

--- server.js
+++ server.js
@@ -2,30 +2,42 @@ var express = require('express');
 var app = express(),
     morgan = require('morgan'),
     mongoose = require('mongoose'),
+    session = require('express-session'),
+    passport = require('passport'),
+    twitter = require('./auth/twitter'),
+    User = require('./models/users');
     port = process.env.PORT || 8080,
     ip = process.env.IP;
 
 require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI);
-var userSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-var User = mongoose.model('User', userSchema);
 
 app.use(morgan('tiny'));
+app.use(session({
+  secret: process.env.TWITTER_SECRET,
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  var user = req.user || '';
+  res.render('index', { user: user });
 });
 
-app.get('/all_polls', function(req, res) {
-  res.render('all_polls');
+app.get('/auth/twitter', twitter.authenticate('twitter'));
+app.get('/auth/twitter/callback', twitter.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
+  res.redirect('/');
 });
 
-app.get('/add', function(req, res) {
+app.get('/login', function(req, res) {
+  res.render('login');
+});
+
+app.get('/add', isLoggedIn, function(req, res) {
   res.render('add');
 });
 
@@ -40,6 +52,19 @@ app.get('/my_polls', function(req, res) {
 app.get('/vote_poll', function(req, res) {
   res.render('/vote_poll');
 });
+
+app.get('/logout', function(req, res) {
+  req.logout();
+  res.redirect('/')
+});
+
+function isLoggedIn(req, res, next) {
+  console.log(req.user);
+  if (req.user) {
+    return next();
+  }
+  res.redirect('/login');
+}
 
 app.listen(port, ip, function() {
   console.log('Server started at port ' + port);
