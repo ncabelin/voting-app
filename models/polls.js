06235357d1b31d6116ddd57ba680833756ad021c
Michael Cabelin <ncabelin@gmail.com>
2017-08-02 01:45:50 -0700

    link models

--- models/polls.js
+++ models/polls.js
@@ -1,14 +1,21 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var PollOptions = require('../models/poll-options');
 
 var Poll = new Schema({
   user: {
     id: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: 'twitter_users'
+      ref: 'Twitter_users'
     }
   },
-  question: String
+  question: String,
+  options: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'PollOptions'
+    }
+  ]
 });
 
 module.exports = mongoose.model('Polls', Poll);
--- models/users.js
+++ models/users.js
@@ -1,9 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
+var Polls = require('../models/polls');
 
 var User = new Schema({
   name: String,
-  someId: String
+  someId: String,
+  polls: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Polls'
+    }
+  ]
 });
 
-module.exports = mongoose.model('twitter_users', User);
+module.exports = mongoose.model('Twitter_users', User);
