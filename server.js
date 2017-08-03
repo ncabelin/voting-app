8c8cc9127abfa813d3f9a8d46cbd9314d2ef4d39
Michael Cabelin <ncabelin@gmail.com>
2017-08-03 15:16:50 -0700

    refactor routes

--- server.js
+++ server.js
@@ -4,14 +4,11 @@ var app = express(),
     mongoose = require('mongoose'),
     session = require('express-session'),
     passport = require('passport'),
-    twitter = require('./auth/twitter'),
     bodyParser = require('body-parser'),
-    Poll = require('./models/polls'),
-    PollOption = require('./models/poll-options.js'),
-    IpAddress = require('./models/ip-address.js'),
-    User = require('./models/users');
     port = process.env.PORT || 8080,
     ip = process.env.IP;
+
+var routes = require('./routes/routes');
 
 require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI);
@@ -28,162 +25,7 @@ app.use(passport.session());
 app.set('view engine', 'ejs');
 app.use(express.static('static'));
 
-app.get('/', function(req, res) {
-  Poll.find({}, function(err, polls) {
-    if (err) {
-      res.status(400).json({'message':'error retrieving polls'})
-    } else {
-      res.render('index', {
-        user: req.user || '',
-        polls: polls || []
-      });
-    }
-  });
-});
-
-app.get('/auth/twitter', twitter.authenticate('twitter'));
-app.get('/auth/twitter/callback', twitter.authenticate('twitter', { failureRedirect: '/login' }), function(req, res) {
-  res.redirect('/');
-});
-
-app.get('/login', function(req, res) {
-  res.render('login', { user: req.user || '' });
-});
-
-app.get('/add', isLoggedIn, function(req, res) {
-  res.render('add', { user: req.user || '' });
-}).post('/add', isLoggedIn, function(req, res) {
-  var question = req.body.question;
-      options = req.body.options.split('\r\n');
-  Poll.create({
-    user: {
-      id: req.user._id
-    },
-    question: question
-  }, function(err, poll) {
-    if (err) {
-      return res.status(400).json({'message': 'error saving poll'});
-    } else {
-      User.findOne({ _id: req.user._id }, function(err, foundUser) {
-        if (err) {
-          return res.status(400).json({'message':'User not found'});
-        } else {
-          foundUser.polls.push(poll);
-          foundUser.save(function(err, user) {
-            if (err) { return res.status(400) }
-            else {
-              options.forEach(function(option) {
-                PollOption.create({
-                  poll: {
-                    id: poll._id
-                  },
-                  option: option,
-                  count: 0
-                }, function(err, pollOption) {
-                  if (err) {
-                    return res.status(400).json({'message': 'error saving option'});
-                  } else {
-                    Poll.findOne({ _id: poll._id }, function(err, foundPoll) {
-                      if (err) {
-                        return res.status(400).json({'message': 'error finding poll'});
-                      } else {
-                        foundPoll.options.push(pollOption);
-                        foundPoll.save(function(err, poll) {
-                          if (err) { return res.status(400) }
-                        });
-                      }
-                    });
-                  }
-                });
-              });
-              res.redirect('/my_polls');
-            }
-          });
-        }
-      });
-    }
-  });
-})
-
-app.get('/edit_delete/:poll_id', isLoggedIn, function(req, res) {
-  res.render('edit_delete');
-});
-
-app.get('/my_polls', isLoggedIn, function(req, res) {
-  User.findOne({ _id: req.user._id })
-    .populate('polls')
-    .exec(function(err, user) {
-      res.render('my_polls', {
-        user: req.user || '',
-        polls: user.polls || []
-      });
-    })
-});
-
-app.get('/vote_poll/:poll_id', function(req, res) {
-  Poll.findOne({ _id: req.params.poll_id })
-    .populate('options')
-    .exec(function(err, foundPoll) {
-      res.render('vote_poll', {
-        user: req.user || '',
-        poll: foundPoll
-      });
-    })
-  })
-  .post('/vote_poll/:poll_id', function(req, res) {
-    // look for ip
-    var ip = req.ip,
-        poll_id = req.params.poll_id;
-        vote_id = req.body.vote_id;
-    IpAddress.findOne({ ip: ip}, function(err, ipadd) {
-      if (err) { res.status(400).json({'error':'error finding ip'})}
-      if (ipadd) {
-        // check if ipadd has the poll id
-        var index = ipadd.polls.indexOf(poll_id);
-        if (index === -1) {
-          // add poll id to ip address
-          ipadd.polls.push(poll_id);
-          ipadd.save();
-          // increment vote to poll-option
-          PollOption.findOne({ _id: vote_id }, function(err, poll_option) {
-            if (err) { res.status(400).json({'message':'error finding poll option'})}
-            poll_option.count += 1;
-            poll_option.save();
-            res.redirect('/vote_poll/' + poll_id);
-          });
-        } else {
-          // else ip found
-          res.status(400).json({'message':'Sorry this ip address cannot vote more than once in this poll'});
-        }
-      } else {
-        // no existing ip address, create new ip address
-        var newIp = new IpAddress({
-          ip: ip,
-          polls: [poll_id]
-        })
-        newIp.save();
-        // increment vote to poll-option
-        PollOption.findOne({ _id: vote_id }, function(err, poll_option) {
-          if (err) { res.status(400).json({'message':'error finding poll option'})}
-          poll_option.count += 1;
-          poll_option.save();
-          res.redirect('/vote_poll/' + poll_id);
-        });
-      }
-    });
-  });
-
-app.get('/logout', isLoggedIn, function(req, res) {
-  req.logout();
-  res.redirect('/')
-});
-
-function isLoggedIn(req, res, next) {
-  if (req.user) {
-    return next();
-  }
-  res.redirect('/login');
-}
+app.use(routes);
 
 app.listen(port, ip, function() {
   console.log('Server started at port ' + port);
